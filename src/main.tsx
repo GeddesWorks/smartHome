afaf00557207b49d26abc38be8ddafa452c48a33
collin <collin@geddesworks.com>
2025-10-07 21:58:28 -0500

    Make legacy again

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
+import "whatwg-fetch";
+import "url-polyfill";
+import 'core-js/web/url';
+import 'core-js/web/url-search-params';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import eruda from "eruda";
+
+// Enable mobile console for debugging
+eruda.init();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
